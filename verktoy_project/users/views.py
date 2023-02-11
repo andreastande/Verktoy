8bb8a4487ef7cf93f00c2e6a48d56ec75af2a361
ragnhifb <ragnhifb@stud.ntnu.no>
2023-02-11 13:38:06 +0100

    #13 Consistent simple UI

--- verktoy_project/users/views.py
+++ verktoy_project/users/views.py
@@ -11,6 +11,7 @@ from django.contrib import messages
 
 #https://docs.djangoproject.com/en/4.1/topics/auth/default/#how-to-log-a-user-in
 #https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+
 #Henter siden for å registrere ny bruker
 def signup(request):
     if request.method == 'POST': #når man sender skjema
