6610b870039b4a931c3cf60c9ad01b57fa4a97a5
Trym Eidsten <trymme@ntnu.no>
2023-02-16 13:42:23 +0100

    Add profile for other than logged in user #18
    Very basic page for building upon later. Gets the profile of the user at users/profile/username

--- verktoy_project/users/urls.py
+++ verktoy_project/users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('profile', views.my_profile, name='my_profile'),
     path('profile/update', views.update_profile, name='update_profile'),
-
+    path('profile/<str:userstring>', views.profile, name='profile'),
 ]
