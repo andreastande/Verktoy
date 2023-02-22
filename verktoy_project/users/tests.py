ceb5d9da3c4a937b2b5a57c7a27ca6d8abb94458
andretan <andretan@stud.ntnu.no>
2023-02-22 19:07:57 +0100

    #21 created tests for the User-class
    All tests pass
    
    Co-authored-by: ingrrot <ingrrot@stud.ntnu.no>

--- verktoy_project/users/tests.py
+++ verktoy_project/users/tests.py
@@ -1,3 +1,22 @@
+from django.db import IntegrityError
 from django.test import TestCase
+from homepage.models import Listing
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
 
 # Create your tests here.
+
+class UserTest(TestCase):
+
+    def test_createUser(self):
+        user1 = User.objects.create_user(username="OlaNordmann", password="jeglikerkoding")
+        user2 = User.objects.create_user(username="JensMuri", password="gløshaugen")
+
+    def test_createSameUserButDiffPassword(self):
+        user1 = User.objects.create_user(username="OlaNordmann", password="jeglikerkoding")
+        try:
+            user2 = User.objects.create_user(username="OlaNordmann", password="jeglikerkoding")
+            self.fail
+        except IntegrityError:
+            pass
