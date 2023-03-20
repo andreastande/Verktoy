5b21d13b5eeb3e30bf9c4988cdbfc0ef0b766121
Trym Eidsten <trymme@ntnu.no>
2023-03-20 10:41:41 +0100

    Create model and form for Review

--- verktoy_project/homepage/admin.py
+++ verktoy_project/homepage/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Listing, Agreement, AgreementRequest, UserDefinedList
+from .models import Listing, Agreement, AgreementRequest, UserDefinedList, Review
 
 # Register your models here.
 
@@ -8,4 +8,5 @@ admin.site.register(Listing)
 admin.site.register(Agreement)
 admin.site.register(AgreementRequest)
 admin.site.register(UserDefinedList)
+admin.site.register(Review)
 
